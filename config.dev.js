ccb4f582237ce6948e06bdf43af39faa2981b570
slametsampon <slamet@pon.co.id>
2024-12-19 20:39:37 +0700

    update HOME: '/SmartActLink/'

--- config.dev.js
+++ config.dev.js
@@ -1,6 +1,7 @@
 const GLOBAL_ENV = {
   NODE_ENV: 'dev',
-  HOME: 'http://127.0.0.1:5500/',
+  //HOME: '/',
+  HOME: '/SmartActLink/',
   //HOME: 'https://slametsampon.github.io/SmartActLink/',
   APP_NAME: 'smartactlink',
   VER: '1.0.0',
