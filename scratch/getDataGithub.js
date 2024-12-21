a322d12ad52fee3b0bf0f294bd5e0dec1126a2cc
slametsampon <slamet@pon.co.id>
2024-12-21 13:17:15 +0700

    format data

--- scratch/getDataGithub.js
+++ scratch/getDataGithub.js
@@ -1,7 +1,7 @@
 import fetchDataGithub from '../utils/fetchDataGithub.js';
 import GLOBAL_ENV from '../config.dev.js';
 async function main() {
-  let jsonUrl = GLOBAL_ENV.JSON_CONFIG_URL_GITHUB;
+  let jsonUrl = GLOBAL_ENV.JSON_OPERATION_URL_GITHUB;
   let jsonData = await fetchDataGithub(jsonUrl);
   if (jsonData) {
     console.log('Data:', jsonData);
@@ -9,7 +9,19 @@ async function main() {
       if (typeof jsonData[key] === 'object' && !Array.isArray(jsonData[key])) {
         console.log(`${key}:`);
       } else {
-        console.log(`${key}: ${data[key]}`);
+        console.log(`${key}: ${jsonData[key]}`);
+      }
+    }
+  }
+  jsonUrl = GLOBAL_ENV.JSON_CONFIG_URL_GITHUB;
+  jsonData = await fetchDataGithub(jsonUrl);
+  if (jsonData) {
+    console.log('Data:', jsonData);
+    for (const key in jsonData) {
+      if (typeof jsonData[key] === 'object' && !Array.isArray(jsonData[key])) {
+        console.log(`${key}:`);
+      } else {
+        console.log(`${key}: ${jsonData[key]}`);
       }
     }
   }
