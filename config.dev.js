45a101b25fc3d695ffe756042d28e431ddd8dd90
slamet slamet <slamet@pon.co.id>
2024-12-23 20:37:25 +0700

    Update config.dev.js

--- config.dev.js
+++ config.dev.js
@@ -2,8 +2,8 @@ const GLOBAL_ENV = {
   NODE_ENV: 'dev',
   VER: '1.0.0',
   //HOME: '/',
-  //HOME: 'https://slametsampon.github.io/SmartActLink/',
-  HOME: 'SmartActLink/',
+  HOME: 'https://slametsampon.github.io/SmartActLink/',
+  //HOME: 'SmartActLink/',
   APP_NAME: 'smartactlink',
   JSON_CONFIG_URL_GITHUB:
     'https://raw.githubusercontent.com/slametsampon/SmartActLink/refs/heads/main/data/configDevices.json',
